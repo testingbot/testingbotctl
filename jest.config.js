651ad6be620b401966f4aac339ab9149a77a91b1
Jochen Delabie <jochen@testingbot.com>
2025-12-04 11:05:04 +0100

    update jest config

--- jest.config.js
+++ jest.config.js
@@ -2,18 +2,8 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   transform: {
-    '^.+\\.(ts|tsx)$': [
-      'ts-jest',
-      {
-        isolatedModules: true, // This is an example option; you can adjust based on your needs
-      },
-    ],
+    '^.+\\.(ts|tsx)$': ['ts-jest', {}],
   },
-  testMatch: ['**/tests/**/*.test.ts'], // Adjust this based on your test folder structure
+  testMatch: ['**/tests/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-  globals: {
-    'ts-jest': {
-      isolatedModules: true, // Helps for faster builds
-    },
-  },
 };
