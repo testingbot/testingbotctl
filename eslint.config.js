c47f3f04394a5c683818ca1082bc7b4252f5c9e3
Jochen Delabie <jochen@testingbot.com>
2025-12-06 13:17:54 +0100

    update eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+const prettierPlugin = require('eslint-plugin-prettier');
+
+module.exports = tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+);
